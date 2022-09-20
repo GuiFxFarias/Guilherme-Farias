9b71236802e791746f988fdcc56f71a8683fe94e
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-20 08:24:13 -0300

    update-css

--- script2.js
+++ script2.js
@@ -3,55 +3,55 @@ let c = document.getElementById('n2')
 let e = document.getElementById('n1')
 
 function entrarl(){
-    a.style.width = '25px'
-    a.style.height = '25px'
-    c.style.height = '20px'
-    c.style.width = '20px'
-    e.style.height = '20px'
-    e.style.width = '20px'
-}
-
-function sairl(){
-    a.style.width = '20px'
-    a.style.height = '20px'
-    c.style.height = '20px'
-    c.style.width = '20px'
-    e.style.height = '20px'
-    e.style.width = '20px'
-}
-
-function entrari(){   
-    a.style.width = '20px'
-    a.style.height = '20px'
+    a.style.width = '30px'
+    a.style.height = '30px'
     c.style.height = '25px'
     c.style.width = '25px'
-    e.style.height = '20px'
-    e.style.width = '20px'
-}
-
-function sairi(){
-    a.style.width = '20px'
-    a.style.height = '20px'
-    c.style.height = '20px'
-    c.style.width = '20px'
-    e.style.height = '20px'
-    e.style.width = '20px'
-}
-
-function entrarg(){
-    a.style.width = '20px'
-    a.style.height = '20px'
-    c.style.height = '20px'
-    c.style.width = '20px'
     e.style.height = '25px'
     e.style.width = '25px'
 }
 
+function sairl(){
+    a.style.width = '25px'
+    a.style.height = '25px'
+    c.style.height = '25px'
+    c.style.width = '25px'
+    e.style.height = '25px'
+    e.style.width = '25px'
+}
+
+function entrari(){   
+    a.style.width = '25px'
+    a.style.height = '25px'
+    c.style.height = '30px'
+    c.style.width = '30px'
+    e.style.height = '25px'
+    e.style.width = '25px'
+}
+
+function sairi(){
+    a.style.width = '25px'
+    a.style.height = '25px'
+    c.style.height = '25px'
+    c.style.width = '25px'
+    e.style.height = '25px'
+    e.style.width = '25px'
+}
+
+function entrarg(){
+    a.style.width = '25px'
+    a.style.height = '25px'
+    c.style.height = '25px'
+    c.style.width = '25px'
+    e.style.height = '30px'
+    e.style.width = '30px'
+}
+
 function sairg(){   
-    a.style.width = '20px'
-    a.style.height = '20px'
-    c.style.height = '20px'
-    c.style.width = '20px'
-    e.style.height = '20px'
-    e.style.width = '20px'
+    a.style.width = '25px'
+    a.style.height = '25px'
+    c.style.height = '25px'
+    c.style.width = '25px'
+    e.style.height = '25px'
+    e.style.width = '25px'
 }
